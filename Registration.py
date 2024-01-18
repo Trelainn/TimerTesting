befcbfbc6337749c2e996c615adfa8802805e02e
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-01-18 11:29:48 -0600

    Changes to registration

--- Registration.py
+++ Registration.py
@@ -34,8 +34,8 @@ if __name__ == '__main__':
 
 
     info = ServiceInfo(
-        "Seamaze_"+hostname+"._tcp.local.",
         "Seamaze_Timer._tcp.local.",
+        "Seamaze_"+hostname+"._tcp.local.",
         addresses=[socket.inet_aton(IPAddress)],
         port=80,
     )
