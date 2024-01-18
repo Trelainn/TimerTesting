7afd8041d0d28617dcba1b7b5d04af06cc27b7ae
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-01-18 12:41:56 -0600

    Changes to registration

--- Registration.py
+++ Registration.py
@@ -12,21 +12,20 @@ if __name__ == '__main__':
     hostname=str(socket.gethostname())
     IPAddress=socket.gethostbyname(hostname+".local")
 
-    info = ServiceInfo(
-        "_seamaze-timer._tcp.local.",
-        "SeamazeTimer"+hostname+"._seamaze-timer._tcp.local.",
-        addresses=[socket.inet_aton(IPAddress)],
-        port=80,
-        properties={'Version': '1.0', 'Nickname': 'Seamaze Timer 1'},
-        server=hostname+".local."
-    )
-
-    zeroconf = Zeroconf(ip_version=IPVersion.All)
-    print("Registration of a service, press Ctrl-C to exit...")
-    zeroconf.register_service(info)
-    
     try:
-        while True:        
+        while True:   
+            info = ServiceInfo(
+                "_seamaze-timer._tcp.local.",
+                "SeamazeTimer"+hostname+"._seamaze-timer._tcp.local.",
+                addresses=[socket.inet_aton(IPAddress)],
+                port=80,
+                properties={'Version': '1.0', 'Nickname': 'Seamaze Timer 1'},
+                server=hostname+".local."
+            )
+
+            zeroconf = Zeroconf(ip_version=IPVersion.All)
+            print("Registration of a service, press Ctrl-C to exit...")
+            zeroconf.register_service(info)     
             sleep(1)
     except KeyboardInterrupt:
         pass
