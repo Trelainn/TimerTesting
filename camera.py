9fd516b8f56d7674d221e1244653934d64f6503c
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-01-27 21:13:23 -0600

    Modified camera.py

--- camera.py
+++ camera.py
@@ -8,10 +8,10 @@ import time
 from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
+picam2.start_preview(Preview.DRM)
 
 preview_config = picam2.create_preview_configuration()
 picam2.configure(preview_config)
 
 picam2.start()
-time.sleep(5)
+time.sleep(60)
