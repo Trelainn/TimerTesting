ccbaed77ca5b79e12d90ca75b58e6c53e622b96d
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-01-27 21:38:17 -0600

    Modified camera.py

--- camera.py
+++ camera.py
@@ -6,23 +6,11 @@
 import time
 
 from picamera2 import Picamera2, Preview
-from picamera2.encoders import H264Encoder
 
 picam2 = Picamera2()
 
 preview_config = picam2.create_preview_configuration(main={"size": (1980, 1080)})
 picam2.configure(preview_config)
-
-video_config = picam2.create_video_configuration(main={"size": (1980, 1080)})
-picam2.configure(video_config)
-
-encoder = H264Encoder(10000000)
-
-picam2.start_recording(encoder, 'test.h264')
-time.sleep(10)
-picam2.stop_recording()
-
-'''
 
 picam2.start_preview(Preview.QTGL)
 
@@ -33,4 +21,3 @@ metadata = picam2.capture_file("test.jpg")
 print(metadata)
 
 picam2.close()
-'''
