7b9d01667a56f6f313f0774f3f185f7f0201e668
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-01-27 21:25:34 -0600

    Modified camera.py

--- camera.py
+++ camera.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 
-# Capture a JPEG while still running in the preview mode.
-
+import io
 import time
 
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
-
-preview_config = picam2.create_preview_configuration()
 capture_config = picam2.create_still_configuration()
-picam2.configure(preview_config)
-
+picam2.configure(picam2.create_preview_configuration())
 picam2.start()
-time.sleep(2)
 
-picam2.switch_mode_and_capture_file(capture_config, "test_full.jpg")
+time.sleep(1)
+data = io.BytesIO()
+picam2.capture_file(data, format='jpeg')
+print(data.getbuffer().nbytes)
+
+time.sleep(1)
+data = io.BytesIO()
+picam2.switch_mode_and_capture_file(capture_config, data, format='jpeg')
+print(data.getbuffer().nbytes)
