54c1102e1cd4ca44e58bc3aee29637f2fc553d43
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-01-27 21:28:01 -0600

    Modified camera.py

--- camera.py
+++ camera.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 
-import io
+# Capture a JPEG while still running in the preview mode. When you
+# capture to a file, the return value is the metadata for that image.
+
 import time
 
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
-capture_config = picam2.create_still_configuration()
-picam2.configure(picam2.create_preview_configuration())
+
+preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+picam2.configure(preview_config)
+
+picam2.start_preview(Preview.QTGL)
+
 picam2.start()
+time.sleep(2)
 
-time.sleep(1)
-data = io.BytesIO()
-picam2.capture_file(data, format='jpeg')
-print(data.getbuffer().nbytes)
+metadata = picam2.capture_file("test.jpg")
+print(metadata)
 
-time.sleep(1)
-data = io.BytesIO()
-picam2.switch_mode_and_capture_file(capture_config, data, format='jpeg')
-print(data.getbuffer().nbytes)
+picam2.close()
