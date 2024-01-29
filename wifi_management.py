b82328f9f11e9ea83e0fc974835c6896d3c8952c
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-01-28 20:25:14 -0600

    Added wifi_management.py

---
+++ wifi_management.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+
+def list_wifi_networks():
+    return os.popen("nmcli dev wifi list").read()
+    
+
+scan_results = list_wifi_networks()
+time.sleep(5)
+print(scan_results)
