375ad1a1ed28dfa3de2b789016d7d2ac18083570
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-01-28 20:01:06 -0600

    Added wifi_testing.py

--- wifi_testing.py
+++ wifi_testing.py
@@ -1,13 +1,8 @@
-# import module
-import os
+import wifimangement_linux as wifi 
+import time
 
-# scan available Wifi networks
-os.system('cmd /c "netsh wlan show networks"')
+wifi.off()
+time.sleep(5)
+wifi.on()
 
-# input Wifi name
-name_of_router = input('Enter Name/SSID of the Wifi Network you wish to connect to: ')
-
-# connect to the given wifi network
-os.system(f'''cmd /c "netsh wlan connect name={name_of_router}"''')
-
-print("If you're not yet connected, try connecting to a previously connected SSID again!")
+print(wifi.printpsk())
