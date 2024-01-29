45fe6e0deb066373ee3c5524b182c0c8ad2394ef
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-01-28 20:51:23 -0600

    Modified wifi_management.py

--- wifi_management.py
+++ wifi_management.py
@@ -3,10 +3,22 @@ import time
 
 
 def list_wifi_networks():
-    return os.popen("nmcli dev wifi list").read()
+    scan_results = os.popen("nmcli dev wifi list").read()
+    scan_results.splitlines()
+    scan_results.pop(0)
+    for result in scan_results:
+        result.split(' ')
+        try:
+            while true:
+                result.pop(' ')
+        except Exception as e:
+            print (e)
+            pass
+    return scan_results
+
     
 
 scan_results = list_wifi_networks()
 time.sleep(5)
 
-print(scan_results.splitlines())
+print(scan_results)
