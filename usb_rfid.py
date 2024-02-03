dba01d8f8b315d66132171ec0a92c8bc1a30f541
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-02-02 21:33:10 -0600

    Modified Usb RFID

--- usb_rfid.py
+++ usb_rfid.py
@@ -4,5 +4,5 @@ serialport = serial.Serial("/dev/ttyACM0", 9600, timeout=0.01)
 
 while True:
     reading = serialport.readlines()
-    print (type(reading) is list:)
+    print (type(reading) is list)
 
