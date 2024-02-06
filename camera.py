cb7a31d7966030ead6d312a844a8c5d114d5e7db
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-02-06 12:35:11 -0600

    Modified camera.py

--- camera.py
+++ camera.py
@@ -9,7 +9,7 @@ def create_video(images, video_name):
     frame = images[0]
     height, width, layers = frame.shape
     print(str(width) + ',' + str(height))
-    fourcc = cv2.VideoWriter_fourcc(*'avc1')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video = cv2.VideoWriter(video_name, fourcc, 30, (width, height))
 
     j = 0
