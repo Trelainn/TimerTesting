3d127a94e90580ada53c3aec9e0c9830a094e421
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-02-10 15:18:12 -0600

    Modified camera.py

--- camera.py
+++ camera.py
@@ -4,36 +4,40 @@ import cv2
 import random
 from datetime import datetime
 from picamera2 import Picamera2
+from threading import Thread
 
-def create_video(images, video_name):
-    frame = images[0]
-    height, width, layers = frame.shape
-    now = datetime.now()
-    fourcc = cv2.VideoWriter_fourcc(*'avc1')
-    video = cv2.VideoWriter(video_name, fourcc, 15, (width, height), True)
+class Camera:
+    '''
+    buffer = []
+    camera = None
+    camera_working = False
+    image_limit = 0
+    '''
+    def __init__(self, image_limit = 90, image_width = 640, image_height = 480):
+        self.camera = Picamera2()
+        self.camera.configure(self.camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (image_width, image_height)}))
+        self.camera.start()
+        self.camera_working = False
+        self.buffer = []
+        self.image_limit = image_limit
+        #Thread(target=self.run_camera, args=()).start()
 
-    for image in images:
-        video.write(cv2.cvtColor(image,cv2.COLOR_BGRA2BGR))
-    
-    cv2.destroyAllWindows()
-    video.release()
-    print(datetime.now() - now)
-camera = Picamera2()
-camera.configure(camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (1920, 1080)}))
-camera.start()
+    def create_video(images, video_name):
+        frame = images[0]
+        height, width, layers = frame.shape
+        fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        video = cv2.VideoWriter('videos/'+video_name, fourcc, 15, (width, height), True)
 
-i = 0
-limit = random.randint(100, 101)
-buffer = []
-cont = True
+        for image in images:
+            video.write(cv2.cvtColor(image,cv2.COLOR_BGRA2BGR))
+        
+        cv2.destroyAllWindows()
+        video.release()
 
-while cont:
-    buffer.append(camera.capture_array())
-    if len(buffer) > 90:
-        buffer.pop(0)
-    i = i + 1
-    if i == limit:
-        cont = False
-    cv2.waitKey(1)
-
-create_video(buffer, 'test_video.mp4')
+    def run_camera(self):
+        while True:
+            if self.camera_working:
+                self.buffer.append(self.camera.capture_array())
+                if len(self.buffer) > self.image_limit:
+                    self.buffer.pop(0)
+            cv2.waitKey(1)
