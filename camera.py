86c7ee26ed73eaabf332d18e8823c1b7b179a4a9
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-02-10 17:27:12 -0600

    Modified camera.py

--- camera.py
+++ camera.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
 
 import cv2
-import random
+import time
 from datetime import datetime
 from picamera2 import Picamera2
 from threading import Thread
 from datetime import datetime
 
 class Camera:
-    '''
-    buffer = []
-    camera = None
-    camera_working = False
-    image_limit = 0
-    '''
-    def __init__(self, image_limit = 90, image_width = 640, image_height = 480, fps = 30):
-        self.camera = Picamera2()
-        self.camera.configure(self.camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (image_width, image_height)}))
-        self.camera.start()
-        self.camera_working = False
-        self.system_on = True
-        self.buffer = []
-        self.image_limit = image_limit
-        self.fps = fps
-        Thread(target=self.run_camera, args=()).start()
+
+    def __init__(self, image_limit, image_width, image_height, fps):
+        self.create_camera(self, image_limit=image_limit, image_width=image_width, image_height=image_height, fps=fps)
 
     def create_video(self, images, video_name):
         if images:
@@ -49,3 +36,22 @@ class Camera:
                 if len(self.buffer) > self.image_limit:
                     self.buffer.pop(0)
             cv2.waitKey(1)
+
+    def stop_camera(self):
+        self.camera_working = False
+        self.camera.stop()
+
+    def start_camera(self):
+        self.buffer = []
+        self.camera.start()
+        self.camera_working = True
+
+    def create_camera(self, image_limit = 90, image_width = 640, image_height = 480, fps = 30):
+        self.system_on = False
+        time.sleep(1)
+        self.camera = Picamera2()
+        self.camera.configure(self.camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (image_width, image_height)}))
+        self.system_on = True
+        self.image_limit = image_limit
+        self.fps = fps
+        Thread(target=self.run_camera, args=()).start()
