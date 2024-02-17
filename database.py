4ff71e1830ddf5c82bdd29506df3fbc6d1b50d86
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-02-17 00:07:22 -0600

    Modified database.py

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
 import psycopg2
 from flask import current_app, g
 from flask.cli import with_appcontext
+from os import environ 
 
 def get_db():
 	db = psycopg2.connect(	
 		host = 'localhost',
 		user = 'postgres',
-		password = 'Soporte1.',
-		database = 'seabob')
+		password = '8fnVZpx7i+RT?47',
+		database = 'seamaze')
 	c = db.cursor()
 	return db, c	
 
