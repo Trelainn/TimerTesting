84757d32c09fd5ca55ee48f7acb8d139d43dc32d
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-02-28 03:46:12 -0600

    Modified gpio_test.py

--- gpio_test.py
+++ gpio_test.py
@@ -9,5 +9,17 @@ serialport = serial.Serial("/dev/ttyS0", 9600, timeout=0.01)
 while True:
   reading = serialport.readlines()
   if reading:
-    data = {"Temperature": reading[0].decode().replace('\r', '').replace('\n','')}
+    data = {"Temperature": reading[1].decode().replace('\r', '').replace('\n',''), 
+            "Voltage_C1": reading[3].decode().replace('\r', '').replace('\n',''),
+            "Voltage_C2": reading[5].decode().replace('\r', '').replace('\n',''),
+            "Voltage_C3": reading[7].decode().replace('\r', '').replace('\n',''),
+            "Voltage_C4": reading[9].decode().replace('\r', '').replace('\n',''),
+            "Voltage_Charger": reading[11].decode().replace('\r', '').replace('\n',''),
+            "Button_Pressed": reading[13].decode().replace('\r', '').replace('\n',''),
+            "Raspberry_Connected": reading[15].decode().replace('\r', '').replace('\n',''),
+            "Starting_System": reading[17].decode().replace('\r', '').replace('\n',''),
+            "System_Shut_Down": reading[19].decode().replace('\r', '').replace('\n',''),
+            "Charging": reading[21].decode().replace('\r', '').replace('\n',''),
+            "Fully_Charged": reading[23].decode().replace('\r', '').replace('\n','')  
+            }
     print(data)
