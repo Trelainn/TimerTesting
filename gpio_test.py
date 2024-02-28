ae26eaf911b6b0bdf1441875fd545977c5f589ea
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-02-28 03:48:56 -0600

    Modified gpio_test.py

--- gpio_test.py
+++ gpio_test.py
@@ -9,13 +9,13 @@ serialport = serial.Serial("/dev/ttyS0", 9600, timeout=0.01)
 while True:
   reading = serialport.readlines()
   if reading:
-    data = {"Temperature": reading[1].decode().replace('\r', '').replace('\n',''), 
-            "Voltage_C1": reading[3].decode().replace('\r', '').replace('\n',''),
-            "Voltage_C2": reading[5].decode().replace('\r', '').replace('\n',''),
-            "Voltage_C3": reading[7].decode().replace('\r', '').replace('\n',''),
-            "Voltage_C4": reading[9].decode().replace('\r', '').replace('\n',''),
-            "Voltage_Charger": reading[11].decode().replace('\r', '').replace('\n',''),
-            "Button_Pressed": reading[13].decode().replace('\r', '').replace('\n',''),
+    data = {"Temperature": float(reading[1].decode().replace('\r', '').replace('\n','')), 
+            "Voltage_C1": float(reading[3].decode().replace('\r', '').replace('\n','')),
+            "Voltage_C2": float(reading[5].decode().replace('\r', '').replace('\n','')),
+            "Voltage_C3": float(reading[7].decode().replace('\r', '').replace('\n','')),
+            "Voltage_C4": float(reading[9].decode().replace('\r', '').replace('\n','')),
+            "Voltage_Charger": float(reading[11].decode().replace('\r', '').replace('\n','')),
+            "Button_Pressed": True if int(reading[13].decode().replace('\r', '').replace('\n','')) == 1 else False,
             "Raspberry_Connected": reading[15].decode().replace('\r', '').replace('\n',''),
             "Starting_System": reading[17].decode().replace('\r', '').replace('\n',''),
             "System_Shut_Down": reading[19].decode().replace('\r', '').replace('\n',''),
