13451ee77dbce20c20f6f82689f2dd456e781d21
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-03-01 22:53:55 -0600

    Modified sensors.py

--- sensors.py
+++ sensors.py
@@ -11,7 +11,7 @@ def registerInNetwork():
 def checkStatus():
     while True:
         response = requests.get('http://localhost:8080/status')
-        print(response)
+        print(response.json())
         time.sleep(1)
 
 if __name__ == "__main__":
