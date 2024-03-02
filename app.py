a433e448519a280e3a8d6ccb182674614831832c
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-03-01 23:57:07 -0600

    Modified app.py

--- app.py
+++ app.py
@@ -372,7 +372,7 @@ def update_status():
     internet_available = request.json['Internet_Available']
     led_status = request.json['LED_Status']
     c.execute(
-        'insert into system_tracker (date, battery_percentage, temperature, race_status_ race_number, camera_on, antenna_on, pcb_connection, internet_available, led_status, charger_connected, starting_system, system_shut_down) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', 
+        'insert into system_tracker (date, battery_percentage, temperature, race_status, race_number, camera_on, antenna_on, pcb_connection, internet_available, led_status, charger_connected, starting_system, system_shut_down) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', 
         (datetime.now(),battery_percentage, temperature, status['race_status'], status['next_race_number'], camera_on, True, pcb_connection, internet_available, led_status, charger_connected, starting_system, system_shut_down)
     )
     return {'ok': True} 
