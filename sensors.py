a1b69344703e66936efe30f29437b9c415beb0c6
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-03-14 04:30:43 -0600

    Modified sensors.py

--- sensors.py
+++ sensors.py
@@ -101,6 +101,7 @@ def readRFID():
             reading = serialport.readlines()
             if reading:
                 tag = reading[0].decode().split('\n')[0]
+                print(tag)
                 time_now = datetime.datetime.now()
                 try:
                     if tag in times:
@@ -109,10 +110,10 @@ def readRFID():
                         if time_recorded > lap_threshold:
                             times[tag] = time_now
                             Thread(target=saveLapTime, args=(tag, time_recorded)).start()
+                    else:
+                        times[tag] = time_now
                 except Exception as e:
                     print(e)
-                    times[tag] = time_now
-
         else:
             time.sleep(1)
 
