11bd396ece88db9c684341a6ea7814699d143f7b
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-03-14 05:25:59 -0600

    Modified app.py

--- app.py
+++ app.py
@@ -339,8 +339,9 @@ def record_time():
             db, c = get_db()
             c.execute('select tag, video_permission from race_competitors where tag = %s and race_number = %s', (tag, status['current_race_number']))
             participant = c.fetchone()
+            return participant 
             if participant is not None:
-                video_permission = c.fetchone()[0]
+                video_permission = c.fetchone()[1]
             c.execute('select lap from race_competitors_laps where tag = %s and race_number = %s order by lap desc', (tag, status['current_race_number']))
             lap_number = c.fetchone()
             if lap_number is not None:
