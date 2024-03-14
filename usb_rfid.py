7d21807bca718666833ac9bab13175bf9bcfc33d
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-03-14 02:24:00 -0600

    Modified usb_rfid.py

--- usb_rfid.py
+++ usb_rfid.py
@@ -1,9 +1,12 @@
 import serial
 
-serialport = serial.Serial("/dev/ttyACM0", 9600, timeout=0.01)
-
-while True:
-    reading = serialport.readlines()
-    if reading:
-        print(reading[0].decode().split('\n')[0])
+class Serial_communication:
+    
+    def run():
+        #serialport = serial.Serial("/dev/ttyACM0", 9600, timeout=0.01)
+        serialport = serial.Serial("/dev/ttyUSB0", 9600, timeout=0.01)
+        while True:
+            reading = serialport.readlines()
+            if reading:
+                tag = reading[0].decode().split('\n')[0]
 
