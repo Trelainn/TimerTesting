8f2b4ecbb6c3cff65fe4bbf74d55776fcde635f7
Trelainn <luisalfredo.perezrenteria@trelainn.com>
2024-03-16 02:33:30 -0600

    Modified app.py

--- app.py
+++ app.py
@@ -197,27 +197,39 @@ def reset_system_parameters():
     update_system_parameters(race_number=status['next_race_number'], race_status='no race', user_id=' ', code=' ')
     return get_system_parameters()
 
-@app.route('/race', methods=['POST'])
+@app.route('/race', methods=['POST', 'PUT'])
 def race():
     status = get_system_parameters()
-    try:
-        if status['race_status'] == 'no race' and 'user_id' in request.json:
+    db, c = get_db()
+    if status['race_status'] == 'no race' and 'user_id' in request.json:
+        try:
             user_id = request.json['user_id']
             race_code = ''.join(random.choice(string.ascii_uppercase) for x in range(4))
             race_number = status['next_race_number']
-            race_status = 'configure_race'
-            db, c = get_db()
+            race_status = 'configure_race'  
             c.execute(
                 'insert into races (race_number, user_id, code, race_status) values (%s, %s, %s, %s)', (race_number , user_id, race_code, race_status)
 			)
             update_system_parameters(race_number=race_number+1, race_status=race_status, user_id=user_id, code=race_code)
             db.commit()
             return {'ok': True, 'process': 'Start race', 'status': 'success', 'race_number': race_number}
-        else:
-            return {'ok': False, 'process': 'Start race', 'status': 'failed', 'error': 'race already created'}
-    except Exception as e: 
-        return {'ok': False, "error": str(e)}    
-
+        except Exception as e: 
+            return {'ok': False, "error": str(e)} 
+    elif status['race_status'] == 'configure_race' and 'user_id' in request.json:
+        if status['race_owner'] == request.json()['user_id']:
+            if 'track' in request.json():
+                c.execute(
+                    'update races set track = %s where race_number = %s', (request.json['track'], status['next_race_number']-1)
+                )
+                db.commit()
+                return {'ok': True, 'process': 'Select track', 'status': 'success'} 
+            elif 'race_type' in request.json and 'limit_number' in request.json:
+                c.execute(
+                    'update races set race_type = %s, limit_number = %s where race_number = %s', (request.json['race_type'], request.json['limit_number'], status['next_race_number']-1)
+                )
+                db.commit()
+                return {'ok': True, 'process': 'Race type', 'status': 'success'}
+            
 @app.route('/create_race', methods=['POST'])
 def create_race():
     status = get_system_parameters()
