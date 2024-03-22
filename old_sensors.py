7b9a72ce8a83280c9ad1ba1f133bfa373e87f10f
Trelainn <153951678+Trelainn@users.noreply.github.com>
2024-03-22 17:24:25 -0600

    Modified old_sensors.py

--- old_sensors.py
+++ old_sensors.py
@@ -125,6 +125,8 @@ def updateWifiList():
         time.sleep(15)
 
 def readRFID():
+    #serialport = serial.Serial("/dev/ttyACM0", 9600, timeout=0.01)
+    #serialport = serial.Serial("/dev/ttyUSB0", 9600, timeout=0.01)
     serialport_RFID = serial.Serial("/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0-port0", 9600, timeout=0.01)
     while True:
         reading = serialport_RFID.readlines()
